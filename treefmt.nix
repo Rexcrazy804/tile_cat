bbb8b16555d2e2694ed613f2b8386b307b4c8946
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-06-16 13:13:16 +0530

    remove treefmt.nix

--- treefmt.nix
+++
@@ -1,8 +0,0 @@
-{pkgs, ...}: {
-  projectRootFile = "./.git/config";
-  programs.alejandra.enable = true;
-  programs.rustfmt = {
-    enable = true;
-    package = pkgs.rust-bin.nightly.latest.rustfmt;
-  };
-}
