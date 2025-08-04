68aa6f3062b3c36fc1b766771780583d2f2a6820
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-08-04 15:15:41 +0400

    add rust-analyzer extension to toolchain

--- default.nix
+++ default.nix
@@ -6,16 +6,18 @@
   inherit (pkgs) newScope;
 
   rustToolchain = pkgs.rust-bin.nightly.latest.default.override {
-    extensions = ["rust-src"];
+    extensions = ["rust-src" "rust-analyzer"];
     targets = ["wasm32-unknown-unknown" "x86_64-unknown-linux-gnu"];
   };
 in
   fix (self: {
     packages = makeScope newScope (self': let
       inherit (self') callPackage;
+      craneLib' = callPackage ((sources.crane {inherit pkgs;}) + "/lib") {};
     in {
-      craneLib = (callPackage ((sources.crane {inherit pkgs;}) + "/lib") {}).overrideToolchain rustToolchain;
+      craneLib = craneLib'.overrideToolchain rustToolchain;
       tile-cat = callPackage ./package.nix {};
+      default = self'.tile-cat;
     });
 
     devShells.default = let
